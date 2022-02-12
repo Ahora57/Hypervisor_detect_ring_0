698bf4360adda941a0cf68333b4c4162dd699044
Ahora57 <84189666+Ahora57@users.noreply.github.com>
2022-02-12 16:16:53 +0300

    Fix time attack and check processor

--- HypervisorCheckR0/HypervisorDetect.hpp
+++ HypervisorCheckR0/HypervisorDetect.hpp
@@ -9,7 +9,16 @@ We can't use SEH for manual map driver's
 
 namespace DetectHyp
 {
-	
+	bool ProcIsIntel()
+	{
+		int cpuid[4]{ -1 };
+		__cpuid(cpuid, 0);
+		if (cpuid[2] == 'letn')
+		{
+			return TRUE;
+		}
+		return FALSE;
+	}
 
 	 bool compare_list_cpuid() 
 	{
@@ -24,9 +33,9 @@ namespace DetectHyp
 			(invalid_cpuid_list[1] != valid_cpuid_list[1]) ||
 			(invalid_cpuid_list[2] != valid_cpuid_list[2]) ||
 			(invalid_cpuid_list[3] != valid_cpuid_list[3]))
-			return true;
+			return TRUE;
 
-		return false; 
+		return FALSE; 
 
 
 
@@ -43,129 +52,103 @@ namespace DetectHyp
 
 	  bool   time_attack_rdtsc()
 	{ 
-		_disable();
-		unsigned long  tick1 = 0;
-		unsigned long tick2 = 0;
-		unsigned long avg = 0;
+		uint64_t avg = 0;
 		int cpuInfo[4] = {};
-		for (int i = 0; i < 1377; i++)
+		for (int i = 0; i < 2500; i++)
 		{
-			tick1 = __readmsr(IA32_TIME_STAMP_COUNTER);
+
+			auto currentIrql = __readcr8(); 
+			__writecr8(HIGH_LEVEL);
+
+			_disable();
+			auto tick1 = __readmsr(IA32_TIME_STAMP_COUNTER);
 			__cpuid(cpuInfo, 0);// vm-exit
-			tick2 = __readmsr(IA32_TIME_STAMP_COUNTER);
+			auto tick2 = __readmsr(IA32_TIME_STAMP_COUNTER);
+			_enable();
+
+			__writecr8(currentIrql);
+
 			avg += (tick2 - tick1);
 		}
-		avg /=  1337;
-		_enable();
-		return (avg < 500 && avg > 25) ? false : true; 
+		avg /= 2500;  
+		return (avg > 500 || 25 > avg) ; 
 	}
 
-	 bool hypervisor_informathion()
-	{
-		
-		SYSTEM_HYPERVISOR_QUERY_INFORMATION HypQueryInformathion{ -1 };
-		ULONG retLenth = NULL;
-
-		NtQuerySystemInformation(
-			SystemHypervisorDetailInformation,
-			&HypQueryInformathion,
-			sizeof(SYSTEM_HYPERVISOR_QUERY_INFORMATION),
-			&retLenth
-		);
-
-		return HypQueryInformathion.HypervisorPresent || HypQueryInformathion.HypervisorDebuggingEnabled;
-
-
-
-	}
-
-	 bool time_attack_rdtscp() {
-
-		unsigned int  blabla = 0;
-		DWORD tscp1 = 0;
-		DWORD tscp2 = 0;
-		DWORD avg = 0;
-		INT cpuid[4] = { -1 };
-
-		__cpuid(cpuid, 0x80000001);
-
-		if ((cpuid[3] >> 27) & 1)//check support rdtscp
-		{
-
-			for (INT j = 0; j < 10; j++)
-			{
-
-				tscp1 = __rdtscp(&blabla);
-				//call 3 cpuid for(vm-exxit) normal detect
-				__cpuid(cpuid, 0);
-				__cpuid(cpuid, 0);
-				__cpuid(cpuid, 0);
-				tscp2 = __rdtscp(&blabla);
-
-				avg += tscp2 - tscp1;
-
-				if (avg < 500 && avg > 25)
-					return false;
-
-				else
-					avg = 0;
-
-			}
-			return true;
-		}
-		else
-			return false; //rdtscp dont support
-	}
+	 
+ 
 
 
 	 bool time_attack_MPERF()
 	{
 		 // Some hypervisor just return 0(like:VMware)
 
-		_disable();
-			int cpuid[4]{ -1 };
-			DWORD64  avg{ 0 };
-			for (size_t i = 0; i < 1337; i++)
+		int cpuid[4]{ -1 };
+		uint64_t  avg{ 0 };
+		for (int i = 0; i < 100; i++)
+		{
+			auto currentIrql = __readcr8();
+			__writecr8(HIGH_LEVEL);
+
+			_disable(); 
+			auto tick1 = __readmsr(IA32_MPERF_MSR);
+			__cpuid(cpuid, 0);//call vm-exit
+			auto tick2 = __readmsr(IA32_MPERF_MSR);
+			_enable();
+
+			__writecr8(currentIrql);
+
+			if ( (!tick1 && !tick2 ) || (tick1 == tick2))
 			{
-				auto tick1 = __readmsr(IA32_MPERF_MSR);
-				__cpuid(cpuid, 0);//call vm-exit
-				auto tick2 = __readmsr(IA32_MPERF_MSR);
-				avg += (tick2 - tick1);
+				return true;
 			}
-		_enable();
-		avg /= 1337;
-			return  (0xff < avg) || (0xc > avg);
+
+			avg += (tick2 - tick1);
+		}
+		avg /= 100;   
+		return  (0x4ff < avg) || (0xc > avg);
 	}
 	
 
 
 	 bool time_attack_APERF()
 	{
-		 // Some hypervisor just return 0(like:VMware)
-		DWORD64 avg{ 0 };
+		uint64_t avg{ 0 };
 		int data[4]{ -1 };
-		_disable();
-		for (size_t i = 0; i < 1337; i++)
+		for (size_t i = 0; i < 100; i++)
 		{
-			DWORD64 tick1 = __readmsr(IA32_APERF_MSR) << 32;
+			auto currentIrql = __readcr8();
+			__writecr8(HIGH_LEVEL);
+
+
+			_disable();
+			auto  tick1 = __readmsr(IA32_APERF_MSR);
 			__cpuid(data, 0); //call vm-exit
-			DWORD64 tick2 = __readmsr(IA32_APERF_MSR) << 32; 
+			auto tick2 = __readmsr(IA32_APERF_MSR);
+			_enable();
 
+			__writecr8(currentIrql);
+
+			if ( (!tick1 && !tick2) || (tick1 == tick2))
+			{
+				return true;
+			}
 			avg += (tick2 - tick1);
-
-		} 
-		_enable();
-		avg /= 1337;
-		return   (avg < 0x00000BE30000 ) || (avg > 0x00000FFF0000000);
+		}
+		avg /= 100;   
+		return  (0x4ff < avg) || (0x33 > avg);
 	}
 	
 	 bool lbr_is_virtulazed()
 	{
-		DWORD64 current_value = __readmsr(MSR_DEBUGCTL);//safe current value
-		__writemsr(MSR_DEBUGCTL, DEBUGCTL_LBR | DEBUGCTL_BTF);
-		DWORD64 whatch_write = __readmsr(MSR_DEBUGCTL);
-		__writemsr(MSR_DEBUGCTL, current_value);
-		return (!(whatch_write & DEBUGCTL_LBR));
+		if (ProcIsIntel())
+		{
+			auto current_value = __readmsr(MSR_DEBUGCTL);//safe current value
+			__writemsr(MSR_DEBUGCTL, DEBUGCTL_LBR | DEBUGCTL_BTF);
+			auto whatch_write = __readmsr(MSR_DEBUGCTL);
+			__writemsr(MSR_DEBUGCTL, current_value);
+			return (!(whatch_write & DEBUGCTL_LBR));
+		}
+		return FALSE;
 	}
  
 	 bool lbr_stask_is_virtulazed()
